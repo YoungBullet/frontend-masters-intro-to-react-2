354448caf97b40e55029120e6d4bb57051de00e2
Tom de Bruin <tom.debruin@skybettingandgaming.com>
2016-12-02 17:10:37 +0000

    Detail via API call

--- js/Details.jsx
+++ js/Details.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 import Header from './Header'
 const { shape, string } = React.PropTypes
 
@@ -9,19 +10,37 @@ const Details = React.createClass({
       description: string.isRequired,
       year: string.isRequired,
       poster: string.isRequired,
-      trailer: string.isRequired
+      trailer: string.isRequired,
+      imdbID: string.isRequired
     })
+  },
+  getInitialState () {
+    return {
+      omdbData: {}
+    }
+  },
+  componentDidMount () {
+    axios.get(`https://www.omdbapi.com/?i=${this.props.show.imdbID}`)
+      .then(response => {
+        this.setState({omdbData: response.data})
+      })
+      .catch(error => console.error('Axios Error:', error))
   },
   render () {
     const { title, description, year, poster, trailer } = this.props.show
+    // console.log(this.state.omdbData)
+    const rating = this.state.omdbData.imdbRating ? <h3>{this.state.omdbData.imdbRating}</h3> : <img src='/img/loading.png' alt='Loading' />
+    const actors = this.state.omdbData.Actors ? <p>{this.state.omdbData.Actors}</p> : <img src='/img/loading.png' alt='Loading' />
     return (
       <div className='details'>
         <Header />
         <section>
           <h1>{title}</h1>
           <h2>({year})</h2>
+          {rating}
           <img src={`/img/posters/${poster}`} />
           <p>{description}</p>
+          {actors}
         </section>
         <div>
           <iframe src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`} frameBorder='0' allowFullScreen />
